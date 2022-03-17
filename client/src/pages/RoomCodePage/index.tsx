34da2c78b19c48a2c1564ed2b93f41b6117ef099
AugustinSorel <sorelaugustin@gmail.com>
2022-03-17 20:26:24 +0000

    zustand added

--- client/src/pages/RoomCodePage/index.tsx
+++ client/src/pages/RoomCodePage/index.tsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../shared/components/formElements/Button";
 import Input from "../../shared/components/formElements/Input";
 import Header from "../../shared/components/navigation/Header";
+import useGame from "../../shared/store/useGame";
 import { RoomCodePageContainer } from "./RoomCodePage.styled";
 
 const RoomCodePage = () => {
+  const roomCode = useGame((state) => state.roomCode);
+  const setRoomCode = useGame((state) => state.setRoomCode);
+
+  const navigate = useNavigate();
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomCode(event.target.value);
+  };
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!roomCode) {
+      return;
+    }
+
+    console.log(roomCode);
+    navigate(`/game/${roomCode}`);
+  };
+
   return (
     <>
       <Header title="Please enter a room code" />
 
-      <RoomCodePageContainer>
-        <Input title="game room" type="text" />
+      <RoomCodePageContainer onSubmit={submitHandler}>
+        <Input
+          title="game room"
+          type="text"
+          value={roomCode}
+          onChangeHandler={onChangeHandler}
+        />
         <Button title="play" />
       </RoomCodePageContainer>
     </>
