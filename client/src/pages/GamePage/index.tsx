0e66352c001859a0a3bdc25f4e2f447f0f2558fb
AugustinSorel <sorelaugustin@gmail.com>
2022-03-19 22:48:58 +0000

    Cleaner code

--- client/src/pages/GamePage/index.tsx
+++ client/src/pages/GamePage/index.tsx
@@ -1,9 +1,10 @@
+import Header from "../../shared/components/navigation/Header";
 import PrivateRoute from "../../shared/components/navigation/PrivateRoute";
 
 const GamePage = () => {
   return (
     <PrivateRoute>
-      <div>GamePage</div>
+      <Header title="Game page" />
     </PrivateRoute>
   );
 };
