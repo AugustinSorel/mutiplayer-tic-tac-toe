3f9447c39badeb270d98f0cdf08b343e49b06370
AugustinSorel <sorelaugustin@gmail.com>
2022-03-20 18:20:52 +0000

    grid added

--- client/src/pages/GamePage/GamePage.styled.ts
+++ client/src/pages/GamePage/GamePage.styled.ts
@@ -1,1 +1,18 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
+
+export const GameContainer = styled(motion.main)`
+  margin: auto;
+  background-color: var(--accent-color);
+
+  display: grid;
+  grid-template: repeat(3, 100px) / repeat(3, 100px);
+  grid-gap: 10px;
+`;
+
+export const GameCell = styled(motion.div)`
+  background-color: var(--background-color);
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+`;
--- client/src/shared/components/navigation/PrivateRoute/index.tsx
+++ client/src/shared/components/navigation/PrivateRoute/index.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import useGame from "../../../store/useGame";
 
 interface Props {
-  children: JSX.Element;
+  children: ReactNode;
 }
 
-const PrivateRoute: FC<Props> = ({ children }) => {
+const PrivateRoute: FC<Props> = ({ children }): any => {
   const roomCode = useGame((state) => state.roomCode);
 
   if (roomCode) {
