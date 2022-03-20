f8167fb88945629108e131eb3bbd0f4346ea35cf
AugustinSorel <sorelaugustin@gmail.com>
2022-03-20 20:17:08 +0000

    path added

--- client/src/pages/GamePage/Cell/index.tsx
+++ client/src/pages/GamePage/Cell/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import SvgIcon from "../../../shared/components/formElements/SvgIcon";
 import theme from "../../../shared/styles/theme";
+import SvpPaths from "../../../shared/utils/SvgPaths";
 import { CellStyle } from "./Cell.styled";
 
 interface Props {
@@ -34,13 +35,9 @@ const Cell: FC<Props> = ({
       }}
       onClick={clickHandler}
     >
-      {gameStatus[index] === "X" && (
-        <SvgIcon path="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
-      )}
+      {gameStatus[index] === "X" && <SvgIcon path={SvpPaths.cross} />}
 
-      {gameStatus[index] === "O" && (
-        <SvgIcon path="M12 0c-6.623 0-12 5.377-12 12s5.377 12 12 12 12-5.377 12-12-5.377-12-12-12zm0 22c-5.519 0-10-4.48-10-10 0-5.519 4.481-10 10-10 5.52 0 10 4.481 10 10 0 5.52-4.48 10-10 10z" />
-      )}
+      {gameStatus[index] === "O" && <SvgIcon path={SvpPaths.circle} />}
     </CellStyle>
   );
 };
