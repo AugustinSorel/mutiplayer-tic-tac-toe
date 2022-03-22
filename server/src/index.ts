ad3ff18d68f6837731aeedf3e7585525496f3d83
AugustinSorel <sorelaugustin@gmail.com>
2022-03-22 20:44:11 +0000

    basic socket io added server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import express, { Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server is running on port 5000");
+});
